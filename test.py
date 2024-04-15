98ce5bcf1a96921708f92c021e747f8a7d9fbc80
HJS <jongsoo@g.skku.edu>
2024-04-15 17:20:25 +0900

    update requirements torch

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ import torch
 print(torch.cuda.is_available())
 print(torch.cuda.current_device())
 print(torch.__version__)
+print(torch.version.cuda)
+print(torch.cuda.get_arch_list())
+print(torch.backends.cudnn.version())
